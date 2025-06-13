9576523e193e0857152d5e4f0c1844f567848380
Baris Cankül <147693702+BarisCnkl@users.noreply.github.com>
2025-06-13 15:30:16 +0200

    first commit

---
+++ My-Springboot-17/src/main/java/org/example/myspringboot17/MySpringboot17Application.java
@@ -0,0 +1,14 @@
+package org.example.myspringboot17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class MySpringboot17Application
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MySpringboot17Application.class, args);
+  }
+
+}
---
+++ My-Springboot-17/src/test/java/org/example/myspringboot17/MySpringboot17ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myspringboot17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class MySpringboot17ApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
